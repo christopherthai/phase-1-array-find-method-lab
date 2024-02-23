3fe626288135b14f7ba231fa28c0351c65ca95c0
Christopher Thai <christopherthai.nyli@gmail.com>
2024-02-23 09:47:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const superbowlWin = (record) => {
+
+  let year
+  let winRecord = record.find((element) => element.result === "W")
+
+  if (winRecord == undefined) {
+    year = winRecord
+  } else {
+    year = winRecord.year
+  }
+  return year
+}
+
